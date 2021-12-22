5b0efe38b894c5c4a24f1fca8daff394251212e6
toan tran <thanhtontran115@gmail.com>
2021-12-22 15:43:58 +0700

    edit, filter and delete, reload

--- admincarstore/src/pages/car-management/CarManagement.js
+++ admincarstore/src/pages/car-management/CarManagement.js
@@ -11,7 +11,7 @@ import {Icon} from '@mui/material';
 import TablePagination from '@mui/material/TablePagination';
 import {useState, useEffect, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {getCar} from '../../Redux/reducer/CarReducer';
+import {changeCarStatus, deleteCar, getCar} from '../../Redux/reducer/CarReducer';
 import CarForm from './Component/CarForm/CarForm';
 import Dialog from '@mui/material/Dialog';
 import { getCategory } from '../../Redux/reducer/CategoryReducer';
@@ -38,16 +38,36 @@ const CarManagement = () => {
   const [pageIndex, setPageIndex] = useState(1);
   const [page, setPage] = useState(0);
   const [open, setOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [formType, setFormType] = useState('create');
   const [selectedItem, setSelectedItem] = useState(null);
-  const isOpen  = Boolean(anchorEl);
-  const cars = useSelector(state => state.CarReducer.listCar);
+  const [searchText, setSearchText] = useState('');
+  const carStatus = useSelector(state => state.CarReducer.status);
+  const cars = useSelector(state => {
+    if (!searchText.length) {
+      return state.CarReducer.listCar;
+    } else {
+      return state.CarReducer.listCar.filter(item =>
+        item.name.includes(searchText),
+      );
+    }
+  });
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCar());
+    if(carStatus) {
+      dispatch(changeCarStatus());
+      return;
+    }
+    dispatch(getCar({start: 0, end: 30}));
     dispatch(getCategory());
-  }, [pageIndex, rowsPerPage]);
+  }, [pageIndex, rowsPerPage, carStatus]);
+
+  useEffect(() => {
+    if(!open) {
+      setSelectedItem(null);
+      setFormType('create');
+    }
+  }, [open]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -63,6 +83,14 @@ const CarManagement = () => {
     setOpen(true);
   };
 
+  const handleDelete = name => {
+    dispatch(deleteCar(name));
+  }
+
+  const onSearch = val => {
+    setTimeout(() => setSearchText(val.target.value), 1000);
+  };
+
   const renderNull = () => {
     return cars.length > 0 ? <caption></caption> : <caption><div className='table-null'><img height={100} width={100} src='https://i.pinimg.com/originals/98/4a/0e/984a0eb26ddb184a63ad4f9f53f8efeb.png'/> No data</div></caption>
   }
@@ -71,7 +99,7 @@ const CarManagement = () => {
       <div className="car-management__header">
         <div className="car-management__header__title">Title</div>
         <div className="car-management__header__control">
-          <input />
+          <input onChange={val => onSearch(val)}/>
           <div className="add-button">
             <Icon
               onClick={() => optionClick()}
@@ -115,18 +143,39 @@ const CarManagement = () => {
                 <TableCell align="right">{`$${row.prices ?? '--'}`}</TableCell>
                 <TableCell align="right">{row.color?.length ?? '--'}</TableCell>
                 <TableCell align="right">
-                  <div>
-                  <Icon
-                    baseClassName="fas"
-                    className="fa-ellipsis-vertical"
-                    sx={{fontSize: 18}}
-                  />
-                  <div>
-                    <div>View</div>
-                    <div>Edit</div>
-                    <div>Delete</div>
-                  </div>
-                  
+                <div className="user-option">
+                    <div className="user-option-item edit">
+                      <Icon
+                        baseClassName="fas"
+                        className="fa-pencil"
+                        sx={{fontSize: 18, padding: 3}}
+                        onClick={() => {
+                          setFormType('edit');
+                          setSelectedItem(row);
+                          setOpen(true);
+                        }}
+                      />
+                    </div>
+                    <div className="user-option-item view">
+                      <Icon
+                        baseClassName="fas"
+                        className="fa-info"
+                        sx={{fontSize: 18, padding: 3}}
+                        onClick={() => {
+                          setFormType('detail')
+                          setSelectedItem(row);
+                          setOpen(true);
+                        }}
+                      />
+                    </div>
+                    <div className="user-option-item delete">
+                      <Icon
+                        onClick={() => handleDelete(row.name)}
+                        baseClassName="fas"
+                        className="fa-trash"
+                        sx={{fontSize: 18, padding: 0.5}}
+                      />
+                    </div>
                   </div>
                 </TableCell>
               </TableRow>
@@ -143,22 +192,7 @@ const CarManagement = () => {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </TableContainer>
-      <Dialog open={open}
-        className='car-form'
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description">
-          <div className='car-form--main'>
-            <div className='car-form__header'>
-              <div>Title</div>
-              <Icon onClick={() => setOpen(false)}
-                        baseClassName="fas"
-                        className="fa-xmark"
-                        sx={{fontSize: 24}}
-                      />
-            </div>
-            {CarForm(selectedItem)}
-          </div>
-      </Dialog>
+      {CarForm(selectedItem, formType, setOpen, open)}
     </div>
   );
 };
--- admincarstore/src/pages/car-management/Component/CarForm/CarForm.js
+++ admincarstore/src/pages/car-management/Component/CarForm/CarForm.js
@@ -1,27 +1,39 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './style.scss';
 import {Icon} from '@mui/material';
-import Avatar from '@mui/material/Avatar';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import ColorForm from '../ColorForm/ColorForm';
 import Dialog from '@mui/material/Dialog';
 import {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {getCategory} from '../../../../Redux/reducer/CategoryReducer';
-import {STATUS} from '../../../../Config/Status/Index';
-import {CarDefault} from '../../../../model/car';
-import {getListColor} from '../../../../Redux/reducer/ColorReducer';
 import {validate} from '../../../../helps/validattion';
-import {addCar} from '../../../../Redux/reducer/CarReducer';
+import {addCar, changeCarStatus, editCar, getCar} from '../../../../Redux/reducer/CarReducer';
+import {setColor} from '../../../../Redux/reducer/ColorReducer';
 
-const CarForm = selectedItem => {
+const CarForm = (selectedItem, formType, setOpen, open) => {
   const dispatch = useDispatch();
-  const [open, setOpen] = useState(false);
-  const [cat, setCat] = useState('');
-  const [car, setCar] = useState(selectedItem ?? CarDefault);
-  const listColor = useSelector(state => state.ColorReducer.listColor);
+  const [colorOpen, setColorOpen] = useState(false);
+  const [cat, setCat] = useState(selectedItem?.category ?? '');
+  const [car, setCar] = useState(selectedItem);
+  const carStatus = useSelector(state => state.CarReducer.status);
+  let listColor = useSelector(state => state.ColorReducer.listColor);
   const categories = useSelector(state => state.CategoryReducer.listCategory);
+
+  useEffect(() => {
+    setCar(selectedItem);
+    const color = selectedItem?.color ?? [];
+    dispatch(setColor(color));
+  }, [open]);
+
+  useEffect(() => {
+    if(carStatus) {
+      dispatch(changeCarStatus());
+      dispatch(getCar({start: 0, end: 30}));
+      setOpen(false);
+      return;
+    }
+  }, [carStatus]);
 
   const handleChange = event => {
     setCat(event.target.value);
@@ -29,11 +41,25 @@ const CarForm = selectedItem => {
   };
 
   const changeData = (event, key) => {
-    car[key] = event.target.value;
-    setCar(car);
+    let carChange = {...car, [key]: event.target.value};
+    setCar(carChange);
+  };
+
+  const handleFormType = () => {
+    switch (formType) {
+      case 'create':
+        return 'Create';
+      case 'edit':
+        return 'Edit';
+      case 'detail':
+        return 'Detail';
+      default:
+        return '';
+    }
   };
 
   const handleListColor = () => {
+    console.log(listColor);
     return listColor.map((el, index) => (
       <div
         key={index}
@@ -45,9 +71,40 @@ const CarForm = selectedItem => {
     ));
   };
 
+  const closePopup = () => {
+    setCar(null);
+    dispatch(setColor([]));
+    setOpen(false);
+  };
+
+  const handleConfirm = () => {
+    switch (formType) {
+      case 'create':
+        handleAddCar()
+        break;
+      case 'edit':
+        handleEditCar();
+        break;
+      case 'detail':
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleEditCar = () => {
+    car.listColor = listColor;
+    if (validate(car).length > 0) {
+      console.log('Please fill in all');
+      return;
+    }
+
+    console.log(car)
+    dispatch(editCar(car));
+  };
+
   const handleAddCar = () => {
     car.listColor = listColor;
-    console.log(car);
     if (validate(car).length > 0) {
       console.log('Please fill in all');
       return;
@@ -57,116 +114,134 @@ const CarForm = selectedItem => {
   };
 
   return (
-    <div className="car-form--container">
-      <form className="car-form-content">
-        <div className="car-form-content--left">
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Name</div>
-            <input
-              className="car-form-content__field__input"
-              placeholder="Please enter name"
-              type="text"
-              onChange={value => changeData(value, 'name')}
-            />
-          </div>
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Price</div>
-            <input
-              className="car-form-content__field__input"
-              placeholder="Please enter prices"
-              onChange={value => changeData(value, 'prices')}
-              type="number"
-            />
-          </div>
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Category</div>
-            <Select
-              className="car-form-content__field__input select-list"
-              value={cat}
-              onChange={handleChange}>
-              {categories.map((el, index) => (
-                <MenuItem key={index} value={el.name}>
-                  {el?.name ?? '--'}
-                </MenuItem>
-              ))}
-            </Select>
-          </div>
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Height</div>
-            <input
-              className="car-form-content__field__input"
-              placeholder="Please enter height"
-              onChange={value => changeData(value, 'height')}
-              type="number"
-            />
-          </div>
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Width</div>
-            <input
-              className="car-form-content__field__input"
-              placeholder="Please enter width"
-              onChange={value => changeData(value, 'width')}
-              type="number"
-            />
-          </div>
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Length</div>
-            <input
-              className="car-form-content__field__input"
-              placeholder="Please enter length"
-              onChange={value => changeData(value, 'length')}
-              type="number"
-            />
-          </div>
-        </div>
-        <div className="car-form-content--right">
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label">Description</div>
-            <input
-              className="car-form-content__field__input"
-              placeholder="Please enter description"
-              onChange={value => changeData(value, 'description')}
-              type="text"
-            />
-          </div>
-          <div className="car-form-content__field">
-            <div className="car-form-content__field__label list-color__header">
-              Color
-              <Icon
-                onClick={() => setOpen(true)}
-                baseClassName="fas"
-                className="fa-palette"
-                sx={{fontSize: 18}}
-              />
-            </div>
-            <div>{handleListColor()}</div>
-            {/* <ColorForm></ColorForm> */}
-          </div>
-        </div>
-      </form>
-      <div className="group-btn">
-        <button className="cancel-btn">
-          <div>Cancel</div>
-        </button>
-        <button className="confirm-btn">
-          <div onClick={() => handleAddCar()}>Confirm</div>
-        </button>
-      </div>
-      <Dialog open={open} className="color-form">
-        <div className="color-form--main">
-          <div className="color-form__header">
-            <div>Title</div>
+    <Dialog
+      open={open}
+      className="car-form"
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description">
+      <div className="car-form--main">
+        <div className="car-form--container">
+          <div className="car-form__header">
+            <div>{handleFormType()}</div>
             <Icon
-              onClick={() => setOpen(false)}
+              onClick={() => closePopup()}
               baseClassName="fas"
               className="fa-xmark"
               sx={{fontSize: 24}}
             />
           </div>
-          {ColorForm(setOpen, open)}
+          <form className="car-form-content">
+            <div className="car-form-content--left">
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">Name</div>
+                <input
+                  className="car-form-content__field__input"
+                  placeholder="Please enter name"
+                  type="text"
+                  value={car?.name ?? ''}
+                  disabled ={formType !== 'create'}
+                  onChange={value => changeData(value, 'name')}
+                />
+              </div>
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">Price</div>
+                <input
+                  className="car-form-content__field__input"
+                  placeholder="Please enter prices"
+                  onChange={value => changeData(value, 'prices')}
+                  value={car?.prices ?? ''}
+                  type="number"
+                />
+              </div>
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">Category</div>
+                <Select
+                  className="car-form-content__field__input select-list"
+                  value={cat}
+                  onChange={handleChange}>
+                  {categories.map((el, index) => (
+                    <MenuItem key={index} value={el.name}>
+                      {el?.name ?? '--'}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </div>
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">Height</div>
+                <input
+                  className="car-form-content__field__input"
+                  placeholder="Please enter height"
+                  onChange={value => changeData(value, 'height')}
+                  type="number"
+                  value={car?.height ?? ''}
+                />
+              </div>
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">Width</div>
+                <input
+                  className="car-form-content__field__input"
+                  placeholder="Please enter width"
+                  onChange={value => changeData(value, 'width')}
+                  type="number"
+                  value={car?.width ?? ''}
+                />
+              </div>
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">Length</div>
+                <input
+                  className="car-form-content__field__input"
+                  placeholder="Please enter length"
+                  onChange={value => changeData(value, 'length')}
+                  type="number"
+                  value={car?.length ?? ''}
+                />
+              </div>
+            </div>
+            <div className="car-form-content--right">
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label">
+                  Description
+                </div>
+                <input
+                  className="car-form-content__field__input"
+                  placeholder="Please enter description"
+                  onChange={value => changeData(value, 'description')}
+                  type="text"
+                  value={car?.description ?? ''}
+                />
+              </div>
+              <div className="car-form-content__field">
+                <div className="car-form-content__field__label list-color__header">
+                  Color
+                  <Icon
+                    onClick={() => {
+                      if(formType !== 'create') {
+                        return;
+                      }
+                      setColorOpen(true)
+                    }}
+                    baseClassName="fas"
+                    className="fa-palette"
+                    sx={{fontSize: 18}}
+                  />
+                </div>
+                <div>{handleListColor()}</div>
+                {/* <ColorForm></ColorForm> */}
+              </div>
+            </div>
+          </form>
+          <div className="form-group-btn">
+            <button className="cancel-btn">
+              <div onClick={() => closePopup()}>Cancel</div>
+            </button>
+            <button className="confirm-btn" style={formType === 'detail' ? {display: 'none'} : {}}>
+              <div onClick={() => handleConfirm()}>{handleFormType()}</div>
+            </button>
+          </div>
         </div>
-      </Dialog>
-    </div>
+        {ColorForm(setColorOpen, colorOpen)}
+      </div>
+    </Dialog>
   );
 };
 
